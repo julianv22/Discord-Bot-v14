fcb2df592d300283c6eb1aaaeeed73facd3beae7
julianv22 <julianv2212@gmail.com>
2022-09-08 19:23:39 +0700

    080922

--- slashcommands/context menu/thanks.js
+++ slashcommands/context menu/thanks.js
@@ -3,7 +3,7 @@ const moment = require('moment-timezone');
 const { ContextMenuCommandBuilder, EmbedBuilder, Client, Interaction, ApplicationCommandType } = require('discord.js');
 
 module.exports = {
-  data: new ContextMenuCommandBuilder().setName(`Thanks User`).setType(ApplicationCommandType.User),
+  data: new ContextMenuCommandBuilder().setName(`Thanks`).setType(ApplicationCommandType.User),
   category: 'context menu',
   scooldown: 0,
  
