aafdb6101dec10b8bed0ace8a034a464956ef563
julianv2212 <julianv2212@gmail.com>
2025-05-04 16:44:07 +0000

    serverInfo

--- functions/server/serverInfo.js
+++ functions/server/serverInfo.js
@@ -1,8 +1,16 @@
-const { Client, GuildMember, Interaction, EmbedBuilder, ChannelType, Guild, Message } = require('discord.js');
-const moment = require('moment-timezone');
+const {
+  Client,
+  GuildMember,
+  Interaction,
+  EmbedBuilder,
+  ChannelType,
+  Guild,
+  Message,
+} = require("discord.js");
+const moment = require("moment-timezone");
 
 /** @param {Client} client */
-module.exports = client => {
+module.exports = (client) => {
   /**
    * @param {Guild} guild
    * @param {GuildMember} author
@@ -10,34 +18,68 @@ module.exports = client => {
    * @param {Message} message
    */
   client.serverInfo = async (guild, author, interaction, message) => {
-    const bots = guild.members.cache.filter(m => m.user.bot).size;
-    const channels = guild.channels.cache.filter(c => c.type === ChannelType.GuildText).size;
-    const voices = guild.channels.cache.filter(c => c.type === ChannelType.GuildVoice).size;
+    const bots = guild.members.cache.filter((m) => m.user.bot).size;
+    const channels = guild.channels.cache.filter(
+      (c) => c.type === ChannelType.GuildText
+    ).size;
+    const voices = guild.channels.cache.filter(
+      (c) => c.type === ChannelType.GuildVoice
+    ).size;
+    const categories = guild.channels.cache.filter(
+      (c) => c.type === ChannelType.GuildCategory
+    ).size;
 
     var embed = new EmbedBuilder()
       .setAuthor({ name: guild.name, iconURL: guild.iconURL(true) })
-      .setTitle('⚠️ Server Info ⚠️')
-      .setColor('Random')
+      .setTitle("⚠️ Server Info ⚠️")
+      .setColor("Random")
       .setThumbnail(guild.iconURL(true))
-      .setFooter({ text: `Requested by ${author.username}`, iconURL: `${author.displayAvatarURL(true)}` })
+      .setFooter({
+        text: `Requested by ${author.username}`,
+        iconURL: `${author.displayAvatarURL(true)}`,
+      })
       .setTimestamp()
       .addFields([
-        { name: '💎 Server Name:', value: `${guild.name}`, inline: true },
-        { name: '🆔:', value: `||${guild.id}||`, inline: true },
-        { name: '👑 Server Owner:', value: `<@${guild.ownerId}>` },
+        { name: "💎 Server Name:", value: `${guild.name}`, inline: true },
+        { name: "🆔:", value: `||${guild.id}||`, inline: true },
+        { name: "👑 Server Owner:", value: `<@${guild.ownerId}>` },
         {
           name: `📊 Members [${guild.memberCount.toLocaleString()}]:`,
-          value: `${(guild.memberCount - bots).toLocaleString()} Members\n${bots} Bots`,
+          value: `${(
+            guild.memberCount - bots
+          ).toLocaleString()} Members | ${bots} Bots`,
           inline: true,
         },
-        { name: '📈 Total Channels:', value: `${channels} Text\n${voices} Voice`, inline: true },
-        { name: '📉 Total Roles:', value: `${guild.roles.cache.size}`, inline: true },
-        { name: '🚀 Total Boosts:', value: `${guild.premiumSubscriptionCount}`, inline: true },
-        { name: '🌏 Server Region:', value: `${guild.preferredLocale}`, inline: true },
-        { name: '☑️ Verification Level:', value: `${guild.verificationLevel}`, inline: true },
+        {
+          name: `📈 Channels [${channels + voices}]:`,
+          value: `\`${categories} 📂 | ${channels} 💬 | ${voices} 🔊\``,
+          inline: true,
+        },
+        {
+          name: `📉 Roles [${guild.roles.cache.size}]:`,
+          value: `Highest: ${guild.roles.highest}`,
+          inline: true,
+        },
+        {
+          name: "🚀 Total Boosts:",
+          value: `${guild.premiumSubscriptionCount}`,
+          inline: true,
+        },
+        {
+          name: "🌏 Server Region:",
+          value: `${guild.preferredLocale}`,
+          inline: true,
+        },
+        {
+          name: "☑️ Verification Level:",
+          value: `${guild.verificationLevel}`,
+          inline: true,
+        },
         {
           name: `📆 Created: <t:${parseInt(guild.createdTimestamp / 1000)}:R>`,
-          value: `${moment(guild.createdAt).tz('Asia/Ho_Chi_Minh').format('HH:mm ddd, Do MMMM YYYY')}`,
+          value: `${moment(guild.createdAt)
+            .tz("Asia/Ho_Chi_Minh")
+            .format("HH:mm ddd, Do MMMM YYYY")}`,
         },
       ]);
 
