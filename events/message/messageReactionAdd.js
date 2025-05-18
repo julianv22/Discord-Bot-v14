722ced5f8da01c0d3094978d31c0317515662519
julianv22 <julianv2212@gmail.com>
2025-05-19 00:41:57 +0700

    messageReactionAdd

--- events/message/messageReactionAdd.js
+++ events/message/messageReactionAdd.js
@@ -74,7 +74,7 @@ module.exports = {
       // Nếu có content hoặc embed
       if (embeds.length > 0) {
         await starboardChannel.send({
-          content: `**${count}** \\⭐ in <#${message.channel.id}>:\n${message.content || ' '}`,
+          content: `**${count}** \\⭐ in <#${message.channel.id}>:}`,
           embeds: embeds,
           components: [jumpButton],
         });
